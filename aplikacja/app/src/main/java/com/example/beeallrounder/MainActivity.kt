95a3c4ba5a910f777ed2db760ba34d1605d1745a
Daniel <daniel.r5@tlen.pl>
2022-05-23 16:57:15 +0200

    nav working - starter

--- aplikacja/app/src/main/java/com/example/beeallrounder/MainActivity.kt
+++ aplikacja/app/src/main/java/com/example/beeallrounder/MainActivity.kt
@@ -4,13 +4,30 @@ import android.content.Context
 import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp() : Boolean {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
 //    private fun changeLanguage(language : String) {
--- aplikacja/app/src/main/java/com/example/beeallrounder/MainFragment.kt
+++ aplikacja/app/src/main/java/com/example/beeallrounder/MainFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,16 +36,21 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        //listeners
-        
+        val view : View = inflater!!.inflate(R.layout.fragment_main,container,false)
+
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return view
     }
 
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val fragmentMainToDBMain = view.findViewById<Button>(R.id.fragmentMainToDBMain)
+        fragmentMainToDBMain.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_mainFragment_to_DBMainFragment)
+        }
+    }
 
     companion object {
         /**
